653ff2da053e53a5384451f2fe55a0d5e0582dab
andi_syamsul <80088193+andim53@users.noreply.github.com>
2023-07-04 16:56:45 +0700

    Fix error with no Latex

--- OSNWeb1/OSN/views.py
+++ OSNWeb1/OSN/views.py
@@ -8,3 +8,7 @@ def index(request):
 
     return render(request, 'OSN/index.html', {'posts': posts})
 
+def detail(request, pk):
+    post = Soal.objects.get(pk=pk)
+
+    return render(request, 'OSN/detail.html', {'post': post})
--- OSNWeb1/OSNWeb1/urls.py
+++ OSNWeb1/OSNWeb1/urls.py
@@ -21,4 +21,7 @@ from OSN import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('<int:pk>/', views.detail, name='detail'),
 ]
+
+
